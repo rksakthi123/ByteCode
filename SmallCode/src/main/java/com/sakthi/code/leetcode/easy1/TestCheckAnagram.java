9a7e69ded2f02e09d2787dfb042b5c3ce4383e98
rksakthi123 <sakthivel.kaliyaperuml@xperi.com>
2025-04-05 19:18:43 +0530

    Adding SmallCode Project

---
+++ SmallCode/src/main/java/com/sakthi/code/leetcode/easy1/TestCheckAnagram.java
@@ -0,0 +1,29 @@
+package com.sakthi.code.leetcode.easy1;
+
+import java.util.Arrays;
+
+public class TestCheckAnagram {
+
+	public static void main(String[] args) {
+		TestCheckAnagram obj=new TestCheckAnagram();
+		String str1="abc";
+		String str2="bca";
+		System.out.println(obj.checkAnagram(str1, str2)); 
+		str1="abc";
+		str2="aac";
+		System.out.println(obj.checkAnagram(str1, str2)); 
+
+	}
+	
+	public boolean checkAnagram(String str1,String str2) {
+		char[] charArray1=str1.toCharArray();
+		char[] charArray2=str2.toCharArray();
+		Arrays.sort(charArray1);
+		Arrays.sort(charArray2);
+		if(Arrays.equals(charArray1, charArray2)) {
+			return true;
+		}
+		return false;
+	}
+
+}
