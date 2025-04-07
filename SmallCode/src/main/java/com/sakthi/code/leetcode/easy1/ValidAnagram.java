3bf34c238efcfac6f2c3964e920441f7f1cf7ae5
rksakthi123 <sakthivel.kaliyaperuml@xperi.com>
2025-04-07 18:15:09 +0530

    Base7

--- SmallCode/src/main/java/com/sakthi/code/leetcode/easy1/ValidAnagram.java
+++ SmallCode/src/main/java/com/sakthi/code/leetcode/easy1/ValidAnagram.java
@@ -2,15 +2,34 @@ package com.sakthi.code.leetcode.easy1;
 
 import java.util.Arrays;
 
-public class CheckAnagram {
-
+public class ValidAnagram {
+	/*
+	 * Given two strings s and t, return true if t is an anagram of s, and false
+	 * otherwise.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: s = "anagram", t = "nagaram"
+	 * 
+	 * Output: true
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: s = "rat", t = "car"
+	 * 
+	 * Output: false
+	 */
+	
 	public static void main(String[] args) {
-		CheckAnagram obj=new CheckAnagram();
+		ValidAnagram obj=new ValidAnagram();
 		String str1="abc";
 		String str2="bca";
 		System.out.println(obj.checkAnagram(str1, str2)); 
 		str1="abc";
 		str2="aac";
+		System.out.println(obj.checkAnagram(str1, str2)); 
+		str1="anagram";
+		str2="nagaram";
 		System.out.println(obj.checkAnagram(str1, str2)); 
 
 	}
